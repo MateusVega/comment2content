a94cfe185be1aec410d9b766bcd45ae3bbe7cdea
mateusvega <mateusggvega@gmail.com>
2026-01-28 13:10:54 -0300

    add render port

--- app.py
+++ app.py
@@ -80,5 +80,6 @@ def process_api():
     }
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
     # .\.venv\Scripts\activate
